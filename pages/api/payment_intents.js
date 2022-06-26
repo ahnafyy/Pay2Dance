469f78e89688ffadb565e2e6597e354e802b9078
ahnafyy <yasin011@umn.edu>
2022-06-25 19:12:24 -0500

    Make api payment intent a little better

--- pages/api/payment_intents.js
+++ pages/api/payment_intents.js
@@ -1,16 +1,28 @@
 import Stripe from "stripe";
+
 const stripe = new Stripe(process.env.SECRET_KEY);
 
-/* This is the code for the server side of the application. It is a function that is called when the
-user clicks the pay button. We create a paymentIntent to be fulfilled by stripe! */
 export default async (req, res) => {
   if (req.method === "POST") {
     try {
+      const { amount, customer, receipt_email, metaData, currency } = req.body;
+      // For production-ready applications we recommend not using the
+      // amount directly from the client without verifying it first. This is to
+      // prevent bad actors from changing the total amount on the client before
+      // it gets sent to the server. A good approach is to send the quantity of
+      // a uniquely identifiable product and calculate the total price server-side.
+      // Then, you would only fulfill orders using the quantity you charged for.
+
+      /* This is creating a payment intent. */
       const paymentIntent = await stripe.paymentIntents.create({
         amount,
-        currency: "usd",
+        customer,
+        receipt_email,
+        metaData,
+        currency,
       });
 
+      /* This is sending the client secret to the client. */
       res.status(200).send(paymentIntent.client_secret);
     } catch (err) {
       res.status(500).json({ statusCode: 500, message: err.message });
